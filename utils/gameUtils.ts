371b09e3f4285fe3bddac8e6a19bb695ed1e098a
William Círico <69127474+william-cirico@users.noreply.github.com>
2024-11-14 14:56:29 -0300

    refactor: using Fisher-Yates as shuffle algorithm

--- utils/gameUtils.ts
+++ utils/gameUtils.ts
@@ -13,8 +13,21 @@ const getRandomEmojis = (emojis: string[], count: number): string[] => {
   return [...randomEmojis];
 };
 
+// Fisher-Yates Shuffle
+const shuffle = (array: any[]) => {
+  const copyArray = [...array];
+
+  for (let i = copyArray.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+
+    [copyArray[i], copyArray[j]] = [copyArray[j], copyArray[i]];
+  }
+
+  return copyArray;
+}
+
 export const getInitialCards = () => {
   const cards = getRandomEmojis(EMOJIS, PAIRS_COUNT);
 
-  return cards.concat(cards).sort(() => Math.random() - 0.5);
+  return shuffle(cards.concat(cards));
 };
